51e1500521b2e0a4e513bf9d7987ec5d1043064b
Lokesh Bhardwaj <lokeshbhardwaj978360448@gmail.com>
2026-08-18 19:19:27 +0530

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.js
@@ -0,0 +1,21 @@
+class Solution {
+    hasDuplicate(nums){
+        const map=new Map();
+        for(let i of nums){
+            if(map.has(i)){
+            return true;
+        }
+        map.set(i,true)
+        }
+        return false;
+
+    
+
+    
+    }
+
+
+
+
+    }
+
