55b6aca3073be448d20a94d76accbc170911b96e
Lokesh Bhardwaj <lokeshbhardwaj978360448@gmail.com>
2026-08-18 18:22:57 +0530

    Add: concatenation-of-array - submission-1

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-1.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    getConcatenation(nums) {
+        const ans=[];
+        const n= nums.length;
+
+        for(let i=0; i<n; i++){
+            ans[i]=nums[i];
+            ans[i+n]=nums[i];
+
+        }
+        return ans;
+    
+
+        
+
+    }
+}
