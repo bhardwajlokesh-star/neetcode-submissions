832e16bb647516daa00efaf20b6443e50a0bd0df
Lokesh Bhardwaj <lokeshbhardwaj978360448@gmail.com>
2026-08-18 19:22:48 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.js
@@ -0,0 +1,23 @@
+class Solution{
+  isAnagram(s,t){
+    if(s.length !==t.length){
+      return false;
+    }
+    const map=new Map();
+    for(let char of s ){
+      map.set(char,(map.get(char)||0)+1);
+    }
+
+    for(let char of t){
+      map.set(char,(map.get(char)||0)-1);
+      
+    }
+    for(let value of map.values()){
+      if(value!==0){
+        return false
+      }
+    }
+    return true
+
+  }
+}
